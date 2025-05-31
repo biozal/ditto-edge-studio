5cac0c1ec0cc43b33a67b84c3110d02b45e73374
Aaron LaBeau <80424345+biozal@users.noreply.github.com>
2025-05-31 08:31:25 -0500

    updated to fix warnings

--- SwiftUI/Ditto Edge Studio/Models/DittoAppConfig.swift
+++ SwiftUI/Ditto Edge Studio/Models/DittoAppConfig.swift
@@ -2,62 +2,76 @@ import Foundation
 
 @Observable
 class DittoAppConfig: Decodable {
-    let _id: String
-    let name: String
-    let appId: String
-    let authToken: String
-    let authUrl: String
-    let websocketUrl: String
-    let httpApiUrl: String
-    let httpApiKey: String
-    let mode: String
+  var _id: String
+  var name: String
+  var appId: String
+  var authToken: String
+  var authUrl: String
+  var websocketUrl: String
+  var httpApiUrl: String
+  var httpApiKey: String
+  var mode: String
 
-    init (_ _id: String,
-          name: String,
-          appId: String,
-          authToken: String,
-          authUrl: String,
-          websocketUrl: String,
-          httpApiUrl: String,
-          httpApiKey: String,
-          mode: String = "online") {
-        
-        self._id = _id
-        self.name = name
-        self.appId = appId
-        self.authToken = authToken
-        self.authUrl = authUrl
-        self.websocketUrl = websocketUrl
-        self.httpApiUrl = httpApiUrl
-        self.httpApiKey = httpApiKey
-        self.mode = mode
+  init(
+      _ _id: String,
+      name: String,
+      appId: String,
+      authToken: String,
+      authUrl: String,
+      websocketUrl: String,
+      httpApiUrl: String,
+      httpApiKey: String,
+      mode: String = "online"
+    ) {
+
+      self._id = _id
+      self.name = name
+      self.appId = appId
+      self.authToken = authToken
+      self.authUrl = authUrl
+      self.websocketUrl = websocketUrl
+      self.httpApiUrl = httpApiUrl
+      self.httpApiKey = httpApiKey
+      self.mode = mode
     }
-    
-    init(value: [String: Any?]) {
-        self._id = value["_id"] as! String
-        self.name = value["name"] as! String
-        self.appId = value["appId"] as! String
-        self.authToken = value["authToken"] as! String
-        self.authUrl = value["authUrl"] as! String
-        self.websocketUrl = value["websocketUrl"] as! String
-        self.httpApiUrl = value["httpApiUrl"] as! String
-        self.httpApiKey = value["httpApiKey"] as! String
-        self.mode = value["mode"] as! String
-    }
+  enum CodingKeys: String, CodingKey {
+    case _id
+    case name
+    case appId
+    case authToken
+    case authUrl
+    case websocketUrl
+    case httpApiUrl
+    case httpApiKey
+    case mode
+  }
+
+  required init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+    _id = try container.decode(String.self, forKey: ._id)
+    name = try container.decode(String.self, forKey: .name)
+    appId = try container.decode(String.self, forKey: .appId)
+    authToken = try container.decode(String.self, forKey: .authToken)
+    authUrl = try container.decode(String.self, forKey: .authUrl)
+    websocketUrl = try container.decode(String.self, forKey: .websocketUrl)
+    httpApiUrl = try container.decode(String.self, forKey: .httpApiUrl)
+    httpApiKey = try container.decode(String.self, forKey: .httpApiKey)
+    mode = try container.decode(String.self, forKey: .mode)
+  }
 }
 
 extension DittoAppConfig {
-    static func new() -> DittoAppConfig {
-        return DittoAppConfig(
-            UUID().uuidString,
-            name: "",
-            appId: "",
-            authToken: "",
-            authUrl: "",
-            websocketUrl: "",
-            httpApiUrl: "",
-            httpApiKey: "",
-            mode: "online"
-        )
-    }
+  static func new() -> DittoAppConfig {
+    return DittoAppConfig(
+      UUID().uuidString,
+      name: "",
+      appId: "",
+      authToken: "",
+      authUrl: "",
+      websocketUrl: "",
+      httpApiUrl: "",
+      httpApiKey: "",
+      mode: "online"
+    )
+  }
 }
