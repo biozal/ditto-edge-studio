6d727bafd76b0f390651ec9a5ae98543f7a65144
Aaron LaBeau <alabeau@gmail.com>
2026-02-03 12:00:45 -0600

    updated release for 0.2.4

--- SwiftUI/scripts/create-build.sh
+++ SwiftUI/scripts/create-build.sh
@@ -4,13 +4,13 @@
 mkdir -p ../dmg_temp
 
 # Copy the app to the temporary directory
-cp -R "../build/Edge Debug Helper.app" ../dmg_temp/
+cp -R "Edge Debug Helper.app" ../dmg_temp/
 
 # Create a symbolic link to Applications folder
 ln -s /Applications ../dmg_temp/Applications
 
 # Get the version from the tag
-VERSION="0.1.2"
+VERSION="0.2.2"
 
 hdiutil create -volname "Edge Debug Helper $VERSION" \
           -srcfolder ../dmg_temp \
